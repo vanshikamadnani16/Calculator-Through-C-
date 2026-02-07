fd99d85216605e7c9b227d175a34bf5faba4b9e0
Vanshika Madnani <vanshikamadnani51@gmail.com>
2026-02-07 21:15:05 +0530

    Initial commit

---
+++ tut1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Enter First Number: ";
+    int a;
+    cin>>a;
+    cout<<"Enter Second Number: ";
+    int b;
+    cin>>b;
+    cout<<"The Sum is: "<<a+b<<endl;
+    cout<<"The Product is: "<<a*b<<endl;
+    cout<<"The Difference is: "<<a-b<<endl;
+    cout<<"The Quotient is: "<<(float)a/b<<endl;
+    cout<<"The Remainder is: "<<a%b<<endl;
+    return 0;
+
+}
